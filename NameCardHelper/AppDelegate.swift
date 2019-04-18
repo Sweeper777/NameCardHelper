01fd92c677ed462f29a47bd7ae0658949c59149f
Sweeper777 <sumulang@gmail.com>
2019-04-18 09:08:25 +0100

    configured FirebaseApp

--- NameCardHelper/AppDelegate.swift
+++ NameCardHelper/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  NameCardHelper
-//
-//  Created by Mulang Su on 14/04/2019.
-//  Copyright © 2019 Mulang Su. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
