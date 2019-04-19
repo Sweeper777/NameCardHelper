0b3f5c97a4196fd77d8ce7c590c295d600e75cf2
Sweeper777 <sumulang@gmail.com>
2019-04-19 10:41:49 +0200

    added newPress IBAction

--- NameCardHelper/ViewController.swift
+++ NameCardHelper/ViewController.swift
@@ -11,5 +11,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    @IBAction func newPress() {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            actionSheet.addAction(UIAlertAction(title: "New Card (Photo Library)", style: .default, handler: {
+                [weak self] _ in
+                self?.newCard(sourceType: .photoLibrary)
+            }))
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            actionSheet.addAction(UIAlertAction(title: "New Card (Camera)", style: .default, handler: {
+                [weak self] _ in
+                self?.newCard(sourceType: .camera)
+            }))
+        }
+        actionSheet.addAction(UIAlertAction(title: "New Group", style: .default, handler: {
+            [weak self] _ in
+            self?.newGroup()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
 }
 
