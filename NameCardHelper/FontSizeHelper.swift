95659d5c83cef9e3e92a5009b7d5b602b9ea8a94
Sweeper777 <sumulang@gmail.com>
2019-04-20 19:27:43 +0100

    Create FontSizeHelper.swift

---
+++ NameCardHelper/FontSizeHelper.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+fileprivate func shouldContinueToEnlarge(targetSize: CGSize, currentSize: CGSize) -> Bool {
+    return targetSize.height > currentSize.height && targetSize.width > currentSize.width
+}
+
+func fontSizeThatFits(size: CGSize, text: NSString, font: UIFont) -> CGFloat {
+    var fontToTest = font.withSize(0)
+    var currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
+    var fontSize = CGFloat(0.2)
+    while shouldContinueToEnlarge(targetSize: size, currentSize: currentSize) {
+        fontToTest = fontToTest.withSize(fontSize)
+        currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
+        fontSize += 0.2
+    }
+    return fontSize - 0.2
+}
+
