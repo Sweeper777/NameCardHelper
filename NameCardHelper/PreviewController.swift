cd7d6678a1fc0c7e9373fffbc949162325df62b5
Sweeper777 <sumulang@gmail.com>
2019-04-20 19:28:51 +0100

    added viewDidAppear

--- NameCardHelper/PreviewController.swift
+++ NameCardHelper/PreviewController.swift
@@ -12,4 +12,24 @@ class PreviewController: UIViewController {
         cardView.layer.cornerRadius = 10
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        EZLoadingActivity.show("Generating Preview...", disableUI: false)
+        let vision = Vision.vision()
+        let textRecognizer = vision.onDeviceTextRecognizer()
+        let vImage = VisionImage(image: imageToProcess)
+        textRecognizer.process(vImage) { [weak self] (result, error) in
+            guard error == nil, let result = result else {
+                EZLoadingActivity.hide(false, animated: true)
+                return
+            }
+            guard let nameCard = self?.processVisionText(result) else {
+                EZLoadingActivity.hide(false, animated: true)
+                return
+            }
+            self?.displayNameCard(nameCard)
+            EZLoadingActivity.hide(true, animated: true)
+        }
+    }
+    
 }
