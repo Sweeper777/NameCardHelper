b67812455a8a7ccd49a7e942a546b348612d232b
Sweeper777 <sumulang@gmail.com>
2019-04-21 09:00:28 +0100

    overridden supportedInterfaceOrientations

--- NameCardHelper/PreviewController.swift
+++ NameCardHelper/PreviewController.swift
@@ -87,4 +87,7 @@ class PreviewController: UIViewController {
         }
     }
     
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
