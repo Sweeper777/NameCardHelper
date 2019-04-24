654cce24b7fe2d00c15c18ee61ae3f200259b1dc
Sweeper777 <sumulang@gmail.com>
2019-04-24 11:24:58 +0100

    renamed TextLine to TextBlock

--- NameCardHelper/TextBlock.swift
+++ NameCardHelper/TextBlock.swift
@@ -1,14 +1,14 @@
 import CoreGraphics
 import RealmSwift
 
-class TextLine: Object {
+class TextBlock: Object {
     @objc dynamic var x: Double = 0
     @objc dynamic var y: Double = 0
     @objc dynamic var width: Double = 0
     @objc dynamic var height: Double = 0
     @objc dynamic var text = ""
     
-    let nameCard = LinkingObjects(fromType: NameCard.self, property: "lines")
+    let nameCard = LinkingObjects(fromType: NameCard.self, property: "blocks")
     
     var rect: CGRect {
         return CGRect(x: x, y: y, width: width, height: height)
