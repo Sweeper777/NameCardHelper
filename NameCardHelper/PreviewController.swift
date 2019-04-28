054b9cabe51ec1c2a355895a817939217d299391
Sweeper777 <sumulang@gmail.com>
2019-04-28 08:48:24 +0100

    added fontSize(forString:toFit:)

--- NameCardHelper/PreviewController.swift
+++ NameCardHelper/PreviewController.swift
@@ -113,6 +113,13 @@ class PreviewController: UIViewController {
         return unitChar.size()
     }
     
+    private func fontSize(forString string: String, toFit size: CGSize) -> CGFloat {
+        guard let longestLineCharCount = string.split(separator: "\n").map({ $0.count }).max() else { return 0 }
+        let numberOfLines = string.filter { $0 == "\n" }.count + 1
+        let unitSize = sizeForUnitCharacter()
+        let eachCharacterMaxWidth = size.width / longestLineCharCount.f
+        let eachCharacterMaxHeight = size.height / numberOfLines.f
+        return min(eachCharacterMaxWidth / unitSize.width, eachCharacterMaxHeight / unitSize.height)
     }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
