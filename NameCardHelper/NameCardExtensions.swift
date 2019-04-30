4c4ffc4e8640c1b44374a4250c72aa465c4fe9ec
Sweeper777 <sumulang@gmail.com>
2019-04-30 07:29:39 +0100

    added fontSize(forString:toFit:) in NameCard extension

--- NameCardHelper/NameCardExtensions.swift
+++ NameCardHelper/NameCardExtensions.swift
@@ -6,4 +6,12 @@ extension NameCard {
         return unitChar.size()
     }
     
+    private func fontSize(forString string: String, toFit size: CGSize) -> CGFloat {
+        guard let longestLineCharCount = string.split(separator: "\n").map({ $0.count }).max() else { return 0 }
+        let numberOfLines = string.filter { $0 == "\n" }.count + 1
+        let unitSize = sizeForUnitCharacter()
+        let eachCharacterMaxWidth = size.width / longestLineCharCount.f
+        let eachCharacterMaxHeight = size.height / numberOfLines.f
+        return min(eachCharacterMaxWidth / unitSize.width, eachCharacterMaxHeight / unitSize.height)
+    }
 }
