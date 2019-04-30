9b527b55fe85521461dfcc30c5368d666c20d750
Sweeper777 <sumulang@gmail.com>
2019-04-30 07:28:26 +0100

    added UIColor.toInt extension

--- NameCardHelper/NameCard.swift
+++ NameCardHelper/NameCard.swift
@@ -29,4 +29,20 @@ extension UIColor {
             blue: rgb & 0xFF
         )
     }
+    
+    func toInt() -> Int {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha)
+        let iRed = Int(fRed * 255.0)
+        let iGreen = Int(fGreen * 255.0)
+        let iBlue = Int(fBlue * 255.0)
+        let iAlpha = Int(fAlpha * 255.0)
+        
+        //  (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are blue).
+        let rgb = (iAlpha << 24) + (iRed << 16) + (iGreen << 8) + iBlue
+        return rgb
+    }
 }
