170299f0355d79aeb0c6ae584c06358df3bff4b7
Sweeper777 <sumulang@gmail.com>
2019-04-30 07:27:50 +0100

    Create CardOptionsController.swift

---
+++ NameCardHelper/CardOptionsController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Eureka
+import ViewRow
+import ColorPickerRow
+
+class CardOptionsController: FormViewController {
+    var nameCard: NameCard!
+    var image: UIImage!
+    
+}
