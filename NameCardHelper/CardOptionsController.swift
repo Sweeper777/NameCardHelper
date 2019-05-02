bad1100aae99f1720d587562f21bbbd6b41a7a79
Sweeper777 <sumulang@gmail.com>
2019-05-02 08:07:01 +0100

    added color picker row

--- NameCardHelper/CardOptionsController.swift
+++ NameCardHelper/CardOptionsController.swift
@@ -16,5 +16,55 @@ class CardOptionsController: FormViewController {
             row.value = false
         }
         
+        form +++ ColorPickerRow(tagColor) {
+            row in
+            row.title = NSLocalizedString("Background Color", comment: "")
+            row.value = .white
+            }
+            .cellSetup {
+                cell, row in
+                let palette1 = ColorPalette(name: NSLocalizedString("Reds", comment: ""), palette: [
+                    ColorSpec(hex: "#ffebeb", name: ""),
+                    ColorSpec(hex: "#fed1d1", name: ""),
+                    ColorSpec(hex: "#fe9e9e", name: ""),
+                    ColorSpec(hex: "#e4bcbc", name: ""),
+                    ColorSpec(hex: "#cba7a7", name: "")
+                    ])
+                let palette2 = ColorPalette(name: NSLocalizedString("Yellows", comment: ""), palette: [
+                    ColorSpec(hex: "#fbffeb", name: ""),
+                    ColorSpec(hex: "#fafed1", name: ""),
+                    ColorSpec(hex: "#f5fe9e", name: ""),
+                    ColorSpec(hex: "#e1e4bc", name: ""),
+                    ColorSpec(hex: "#c8cba7", name: "")
+                    ])
+                let palette3 = ColorPalette(name: NSLocalizedString("Greens", comment: ""), palette: [
+                    ColorSpec(hex: "#ebfff0", name: ""),
+                    ColorSpec(hex: "#d1fed3", name: ""),
+                    ColorSpec(hex: "#9efea3", name: ""),
+                    ColorSpec(hex: "#bce4be", name: ""),
+                    ColorSpec(hex: "#a7cba9", name: "")
+                    ])
+                let palette4 = ColorPalette(name: NSLocalizedString("Blues", comment: ""), palette: [
+                    ColorSpec(hex: "#ebf9ff", name: ""),
+                    ColorSpec(hex: "#d1fdfe", name: ""),
+                    ColorSpec(hex: "#9efdfe", name: ""),
+                    ColorSpec(hex: "#bce4e4", name: ""),
+                    ColorSpec(hex: "#a7cacb", name: "")
+                    ])
+                let palette5 = ColorPalette(name: NSLocalizedString("Purples", comment: ""), palette: [
+                    ColorSpec(hex: "#faebff", name: ""),
+                    ColorSpec(hex: "#ead1fe", name: ""),
+                    ColorSpec(hex: "#d49efe", name: ""),
+                    ColorSpec(hex: "#d2bce4", name: ""),
+                    ColorSpec(hex: "#bba7cb", name: "")
+                    ])
+                let palette6 = ColorPalette(name:  NSLocalizedString("Grayscale", comment: ""), palette: [
+                    ColorSpec(hex: "#ffffff", name: ""),
+                    ColorSpec(hex: "#f3f3f3", name: ""),
+                    ColorSpec(hex: "#dadada", name: ""),
+                    ColorSpec(hex: "#c0c0c0", name: "")
+                    ])
+                cell.palettes = [palette1, palette2, palette3, palette4, palette5, palette6]
+        }
     }
 }
