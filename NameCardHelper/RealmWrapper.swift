f9e39a1fced424384afbcfa50ee47e7b41819474
Sweeper777 <sumulang@gmail.com>
2019-05-08 07:08:43 +0100

    added RealmWrapper.init

--- NameCardHelper/RealmWrapper.swift
+++ NameCardHelper/RealmWrapper.swift
@@ -3,4 +3,15 @@ import RealmSwift
 final class RealmWrapper {
     let cards: Results<NameCard>!
     let realm: Realm!
+    
+    private init() {
+        do {
+            realm = try Realm()
+            cards = realm.objects(NameCard.self)
+        } catch let error {
+            print(error)
+            fatalError()
+        }
+    }
+    
 }
