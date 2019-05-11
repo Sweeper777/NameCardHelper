373db36d911cee6d9d7bb4a77224acab5c036a06
Sweeper777 <sumulang@gmail.com>
2019-05-11 11:43:56 +0100

    overridden GroupCollectionCell.isSelected

--- NameCardHelper/GroupCollectionCell.swift
+++ NameCardHelper/GroupCollectionCell.swift
@@ -3,4 +3,17 @@ import UIKit
 class GroupCollectionCell: UICollectionViewCell {
     @IBOutlet var label: UILabel!
     
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                self.layer.cornerRadius = 3
+                self.backgroundColor = .red
+                self.label.textColor = .white
+            } else {
+                self.layer.cornerRadius = 0
+                self.backgroundColor = .clear
+                self.label.textColor = .black
+            }
+        }
+    }
 }
