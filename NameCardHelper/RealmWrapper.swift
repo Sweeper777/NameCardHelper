775285448659ff466b9811cd107425960def8f57
Sweeper777 <sumulang@gmail.com>
2019-05-11 11:03:32 +0100

    added RealmWrapper.groups

--- NameCardHelper/RealmWrapper.swift
+++ NameCardHelper/RealmWrapper.swift
@@ -2,12 +2,14 @@ import RealmSwift
 
 final class RealmWrapper {
     let cards: Results<NameCard>!
+    let groups: Results<Group>!
     let realm: Realm!
     
     private init() {
         do {
             realm = try Realm()
             cards = realm.objects(NameCard.self)
+            groups = realm.objects(Group.self)
         } catch let error {
             print(error)
             fatalError()
