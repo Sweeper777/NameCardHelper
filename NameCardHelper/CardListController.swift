d85afc69a3b1b37e2f7c2027b18f88d04cdef238
Sweeper777 <sumulang@gmail.com>
2019-05-13 16:28:57 +0100

    performed zoom in segue

--- NameCardHelper/CardListController.swift
+++ NameCardHelper/CardListController.swift
@@ -255,18 +255,15 @@ extension CardListController : CircleMenuDelegate {
     }
     
     func circleMenu(_ circleMenu: CircleMenu, buttonDidSelected button: UIButton, atIndex: Int) {
-        guard let cardView = cardCollectionView.indexPathsForSelectedItems?.first.map({ cardCollectionView.cellForItem(at: $0) }) as? UICollectionViewCell else { return }
-//        if cardView.transform == .identity {
-            UIView.animate(withDuration: 0.3) {
-                let heightRatio = self.cardCollectionView.width / cardView.height
-                let widthRatio = self.cardCollectionView.height / cardView.width
-                let scaleFactor = min(heightRatio, widthRatio)
-                cardView.transform = CGAffineTransform(rotationAngle: .pi / 2).scaledBy(x: scaleFactor, y: scaleFactor)
-            }
-//        } else {
-//            UIView.animate(withDuration: 0.3) {
-//                cardView.transform = .identity
-//            }
-//        }
+        guard let selectedCard = self.selectedCard else {
+            
+            // TODO: SCLAlertView
+            
+            return
+        }
+        
+        if atIndex == 3 {
+            performSegue(withIdentifier: "zoomIn", sender: selectedCard)
+        }
     }
 }
