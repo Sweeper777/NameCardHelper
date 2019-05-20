be6432fe85559786edda0680efb3335d6a4b1617
Sweeper777 <sumulang@gmail.com>
2019-05-20 08:18:42 +0100

    used realm results for group collection view data source

--- NameCardHelper/GroupCollectionViewExtensions.swift
+++ NameCardHelper/GroupCollectionViewExtensions.swift
@@ -4,16 +4,14 @@ import UIKit
 
 extension CardListController : UICollectionViewDelegateFlowLayout {
     func groupCollectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //        return RealmWrapper.shared.groups.count + 1
-        return groups.count + 1
+        return RealmWrapper.shared.groups.count + 1
     }
     
     func groupCollectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! GroupCollectionCell
         if indexPath.item != 0 {
-            //            let model = RealmWrapper.shared.groups[indexPath.item - 1]
-            //            cell.label.text = model.name
-            cell.label.text = groups[indexPath.item - 1]
+            let model = RealmWrapper.shared.groups[indexPath.item - 1]
+            cell.label.text = model.name
             cell.label.font = UIFont.systemFont(ofSize: groupLabelFontSize)
         } else {
             cell.label.text = "Ungrouped"
@@ -25,9 +23,8 @@ extension CardListController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == groupCollectionView {
             if indexPath.item != 0 {
-                //                let model = RealmWrapper.shared.groups[indexPath.item - 1]
-                //                let width = (model.name as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: UIFont.systemFontSize)]).width
-                let width = (groups[indexPath.item - 1] as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: groupLabelFontSize)]).width
+                let model = RealmWrapper.shared.groups[indexPath.item - 1]
+                let width = (model.name as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: groupLabelFontSize)]).width
                 return CGSize(width: width + 20, height: collectionView.height)
             } else {
                 let width = ("Ungrouped" as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: groupLabelFontSize)]).width
