c91372d16ef81aa380d6adcb5b1285469252212d
Sweeper777 <sumulang@gmail.com>
2019-05-26 08:40:14 +0100

    added Group.description

--- NameCardHelper/Group.swift
+++ NameCardHelper/Group.swift
@@ -3,4 +3,8 @@ import RealmSwift
 class Group: Object {
     @objc dynamic var name = ""
     let nameCards = List<NameCard>()
+    
+    override var description: String {
+        return name
+    }
 }
