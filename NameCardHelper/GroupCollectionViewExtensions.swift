7b2bf841356ccc7ce619412fcff4dbae43e809d4
Sweeper777 <sumulang@gmail.com>
2019-05-31 08:56:46 +0100

    fixed sizeForItemAt

--- NameCardHelper/GroupCollectionViewExtensions.swift
+++ NameCardHelper/GroupCollectionViewExtensions.swift
@@ -8,14 +8,10 @@ extension CardListController : UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == groupCollectionView {
-            if indexPath.item != 0 {
-                let model = RealmWrapper.shared.groups[indexPath.item - 1]
+                let model: Group = try! collectionView.rx.model(at: indexPath)
                 let width = (model.name as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: groupLabelFontSize)]).width
                 return CGSize(width: width + 20, height: collectionView.height)
-            } else {
-                let width = ("Ungrouped" as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: groupLabelFontSize)]).width
-                return CGSize(width: width + 20, height: collectionView.height)
-            }
+            
         } else {
             return CGSize(width: UIScreen.width, height: UIScreen.width / nameCardWHRatio)
         }
