fad8419043a97d8cc1c9ab32d8209318ec242737
Sweeper777 <sumulang@gmail.com>
2019-06-01 10:54:42 +0100

    added Group.ungrouped

--- NameCardHelper/Group.swift
+++ NameCardHelper/Group.swift
@@ -4,6 +4,12 @@ class Group: Object {
     @objc dynamic var name = ""
     let nameCards = List<NameCard>()
     
+    static let ungrouped: Group = {
+        let retVal = Group()
+        retVal.name = "Ungrouped"
+        return retVal
+    }()
+    
     override var description: String {
         return name
     }
