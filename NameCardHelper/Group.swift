802b7ed63b254a3b1943d1e2a8bfd63272de9244
Sweeper777 <sumulang@gmail.com>
2019-06-01 10:55:38 +0100

    added Group.validateAndShowAlert

--- NameCardHelper/Group.swift
+++ NameCardHelper/Group.swift
@@ -1,4 +1,5 @@
 import RealmSwift
+import SCLAlertView
 
 class Group: Object {
     @objc dynamic var name = ""
@@ -12,5 +13,19 @@ class Group: Object {
     
     override var description: String {
         return name
+    }
+}
+
+extension Group {
+    func validateAndShowAlert() -> Bool {
+        if ["Ungrouped", "New Group", "None", ""].contains(name.trimmed()) ||
+            RealmWrapper.shared.groups.filter("name == %@", name).count > 0 {
+            let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
+            alert.addButton("OK", action: {})
+            alert.showError("Error", subTitle: "You cannot have a group with this name!")
+            return false
+        } else {
+            return true
+        }
     }
 }
