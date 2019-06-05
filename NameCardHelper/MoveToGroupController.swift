d06276bb25bfca8136b8e34d4cd5b28d87d0bf76
Sweeper777 <sumulang@gmail.com>
2019-06-05 06:58:57 +0100

    implemented cellForRowAt

--- NameCardHelper/MoveToGroupController.swift
+++ NameCardHelper/MoveToGroupController.swift
@@ -18,4 +18,17 @@ class MoveToGroupController : UITableViewController {
         return RealmWrapper.shared.groups.count + 1
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "Ungrouped"
+        } else {
+            cell.textLabel?.text = groups[indexPath.row - 1].name
+        }
+        if selectedGroup.name == cell.textLabel!.text {
+            cell.accessoryType = .checkmark
+        }
+        return cell
+    }
+    
 }
