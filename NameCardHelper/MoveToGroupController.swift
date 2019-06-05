8211ab7913be36285f7530ecee6886cb0f79c0c9
Sweeper777 <sumulang@gmail.com>
2019-06-05 06:58:41 +0100

    implemented numberOfSections and numberOfRows

--- NameCardHelper/MoveToGroupController.swift
+++ NameCardHelper/MoveToGroupController.swift
@@ -5,4 +5,17 @@ class MoveToGroupController : UITableViewController {
     var selectedGroup: Group!
     var groups: Results<Group>!
     weak var delegate: MoveToGroupControllerDelegate?
+    
+    override func viewDidLoad() {
+        groups = RealmWrapper.shared.groups.sorted(byKeyPath: "name")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return RealmWrapper.shared.groups.count + 1
+    }
+    
 }
