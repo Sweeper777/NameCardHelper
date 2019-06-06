300f99a7917b422fb3c529521a3099daf183c7c3
Sweeper777 <sumulang@gmail.com>
2019-06-06 17:56:47 +0100

    added extractInfo

--- NameCardHelper/NameCardInfoExtractor.swift
+++ NameCardHelper/NameCardInfoExtractor.swift
@@ -9,4 +9,26 @@ struct ExtractedInfo {
 extension NameCard {
     func extractInfo() -> ExtractedInfo {
     }
+    
+    fileprivate func extractInfo(in result: NSTextCheckingResult?, to contact: CNMutableContact) {
+        if let link = result?.url {
+            if link.scheme == "mailto" {
+                contact.emailAddresses.append(CNLabeledValue(label: "Work", value: (link as NSURL).resourceSpecifier! as NSString))
+            } else {
+                contact.urlAddresses.append(CNLabeledValue(label: "Work", value: link.absoluteString as NSString))
+            }
+        }
+        if let phone = result?.phoneNumber {
+            contact.phoneNumbers.append(CNLabeledValue(label: "Unlabeled", value: CNPhoneNumber(stringValue: phone)))
+        }
+        if let address = result?.addressComponents {
+            let cnAddress = CNMutablePostalAddress()
+            cnAddress.city = address[.city] ?? ""
+            cnAddress.country = address[.country] ?? ""
+            cnAddress.street = address[.street] ?? ""
+            cnAddress.state = address[.state] ?? ""
+            cnAddress.postalCode = address[.zip] ?? ""
+            contact.postalAddresses.append(CNLabeledValue(label: "Company", value: cnAddress))
+        }
+    }
 }
