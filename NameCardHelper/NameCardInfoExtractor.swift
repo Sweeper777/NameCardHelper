c825ca2451a691815572a6bc411bd6b19eb63ccf
Sweeper777 <sumulang@gmail.com>
2019-06-06 17:55:27 +0100

    added ExractedInfo struct

--- NameCardHelper/NameCardInfoExtractor.swift
+++ NameCardHelper/NameCardInfoExtractor.swift
@@ -1,6 +1,10 @@
 import Contacts
 import SwiftyUtils
 
+struct ExtractedInfo {
+    let contact: CNMutableContact
+    let remainingText: [String]
+}
 
 extension NameCard {
     func extractInfo() -> ExtractedInfo {
