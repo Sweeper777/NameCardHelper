e1c243bbf3906c97870a2f76632b29b0ced466e2
Sweeper777 <sumulang@gmail.com>
2019-06-08 12:07:34 +0100

    used GroupStruct in GroupCollectionViewExtensions

--- NameCardHelper/GroupCollectionViewExtensions.swift
+++ NameCardHelper/GroupCollectionViewExtensions.swift
@@ -8,7 +8,7 @@ extension CardListController : UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == groupCollectionView {
-                let model: Group = try! collectionView.rx.model(at: indexPath)
+                let model: GroupStruct = try! collectionView.rx.model(at: indexPath)
                 let width = (model.name as NSString).size(withAttributes: [.font: UIFont.systemFont(ofSize: groupLabelFontSize)]).width
                 return CGSize(width: width + 20, height: collectionView.height)
             
