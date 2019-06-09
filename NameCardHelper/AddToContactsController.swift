ef2c7ca375ee9846099f59feec335f24cda37eb9
Sweeper777 <sumulang@gmail.com>
2019-06-09 08:38:12 +0100

    Create AddToContactsController.swift

---
+++ NameCardHelper/AddToContactsController.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Eureka
+import SuggestionRow
+import SplitRow
+import ViewRow
+
+class AddToContactsController: FormViewController {
+    var nameCard: NameCard!
+    var extractedInfo: ExtractedInfo!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
