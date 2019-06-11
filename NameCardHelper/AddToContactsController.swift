486b1f3e1e42ab60fd982713a0debe5838c55a65
Sweeper777 <sumulang@gmail.com>
2019-06-11 08:15:04 +0100

    added Full Name row

--- NameCardHelper/AddToContactsController.swift
+++ NameCardHelper/AddToContactsController.swift
@@ -3,6 +3,10 @@ import Eureka
 import SuggestionRow
 import SplitRow
 import ViewRow
+import FloatLabelRow
+import PostalAddressRow
+import Contacts
+import ContactsUI
 
 class AddToContactsController: FormViewController {
     var nameCard: NameCard!
@@ -10,6 +14,14 @@ class AddToContactsController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        form +++ Section()
+        <<< SuggestionAccessoryRow<String>(tagFullName) {
+            row in
+            row.title = "Full Name"
+            row.filterFunction = filterFunction
+        }
+        
     func filterFunction(text: String) -> [String] {
         return self.extractedInfo.remainingText.filter { text == "" || $0.contains(text) }
     }
